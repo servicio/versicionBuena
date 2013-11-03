5783e69ef955433714ef269290020d6502cfbd12
Administrador <Administrador@ba-556e4d6b45a7>
2013-11-03 14:02:17 -0600

    joseCambiostutorias

---
+++ ServicioSocial/clases/CuestrionarioTutoria.php
@@ -0,0 +1,196 @@
+<?php
+class CuestrionarioTutoria {
+    private $matricula;
+    private $pregunta1;
+    private $pregunta2;
+    private $pregunta3;
+    private $pregunta4;
+    private $pregunta5;
+    private $pregunta6;
+    private $pregunta7;
+    private $pregunta8;
+    private $pregunta9;
+    private $pregunta10;
+    private $pregunta11;
+    private $pregunta12;
+    private $pregunta13;
+    private $pregunta14;
+    private $pregunta15;
+    private $pregunta16;
+    private $pregunta17;
+    private $pregunta18;
+    private $pregunta19;
+    private $pregunta20;
+    
+    public function getMatricula() {
+        return $this->matricula;
+    }
+
+    public function setMatricula($matricula) {
+        $this->matricula = $matricula;
+    }
+
+    public function getPregunta1() {
+        return $this->pregunta1;
+    }
+
+    public function setPregunta1($pregunta1) {
+        $this->pregunta1 = $pregunta1;
+    }
+
+    public function getPregunta2() {
+        return $this->pregunta2;
+    }
+
+    public function setPregunta2($pregunta2) {
+        $this->pregunta2 = $pregunta2;
+    }
+
+    public function getPregunta3() {
+        return $this->pregunta3;
+    }
+
+    public function setPregunta3($pregunta3) {
+        $this->pregunta3 = $pregunta3;
+    }
+
+    public function getPregunta4() {
+        return $this->pregunta4;
+    }
+
+    public function setPregunta4($pregunta4) {
+        $this->pregunta4 = $pregunta4;
+    }
+
+    public function getPregunta5() {
+        return $this->pregunta5;
+    }
+
+    public function setPregunta5($pregunta5) {
+        $this->pregunta5 = $pregunta5;
+    }
+
+    public function getPregunta6() {
+        return $this->pregunta6;
+    }
+
+    public function setPregunta6($pregunta6) {
+        $this->pregunta6 = $pregunta6;
+    }
+
+    public function getPregunta7() {
+        return $this->pregunta7;
+    }
+
+    public function setPregunta7($pregunta7) {
+        $this->pregunta7 = $pregunta7;
+    }
+
+    public function getPregunta8() {
+        return $this->pregunta8;
+    }
+
+    public function setPregunta8($pregunta8) {
+        $this->pregunta8 = $pregunta8;
+    }
+
+    public function getPregunta9() {
+        return $this->pregunta9;
+    }
+
+    public function setPregunta9($pregunta9) {
+        $this->pregunta9 = $pregunta9;
+    }
+
+    public function getPregunta10() {
+        return $this->pregunta10;
+    }
+
+    public function setPregunta10($pregunta10) {
+        $this->pregunta10 = $pregunta10;
+    }
+
+    public function getPregunta11() {
+        return $this->pregunta11;
+    }
+
+    public function setPregunta11($pregunta11) {
+        $this->pregunta11 = $pregunta11;
+    }
+
+    public function getPregunta12() {
+        return $this->pregunta12;
+    }
+
+    public function setPregunta12($pregunta12) {
+        $this->pregunta12 = $pregunta12;
+    }
+
+    public function getPregunta13() {
+        return $this->pregunta13;
+    }
+
+    public function setPregunta13($pregunta13) {
+        $this->pregunta13 = $pregunta13;
+    }
+
+    public function getPregunta14() {
+        return $this->pregunta14;
+    }
+
+    public function setPregunta14($pregunta14) {
+        $this->pregunta14 = $pregunta14;
+    }
+
+    public function getPregunta15() {
+        return $this->pregunta15;
+    }
+
+    public function setPregunta15($pregunta15) {
+        $this->pregunta15 = $pregunta15;
+    }
+
+    public function getPregunta16() {
+        return $this->pregunta16;
+    }
+
+    public function setPregunta16($pregunta16) {
+        $this->pregunta16 = $pregunta16;
+    }
+
+    public function getPregunta17() {
+        return $this->pregunta17;
+    }
+
+    public function setPregunta17($pregunta17) {
+        $this->pregunta17 = $pregunta17;
+    }
+
+    public function getPregunta18() {
+        return $this->pregunta18;
+    }
+
+    public function setPregunta18($pregunta18) {
+        $this->pregunta18 = $pregunta18;
+    }
+
+    public function getPregunta19() {
+        return $this->pregunta19;
+    }
+
+    public function setPregunta19($pregunta19) {
+        $this->pregunta19 = $pregunta19;
+    }
+
+    public function getPregunta20() {
+        return $this->pregunta20;
+    }
+
+    public function setPregunta20($pregunta20) {
+        $this->pregunta20 = $pregunta20;
+    }
+
+
+}
+
+?>
